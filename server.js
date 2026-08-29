750c80ae54e9b15ca5f1430fd26d197c94c39957
caubengungo <dangntmhe210550@gmail.com>
2026-08-29 19:44:37 +0700

    l

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const PORT = 8383;
 app.get('/', (req, res) => {
     console.log('lmao', req.method);
-    res.sendStatus(200);
+    res.send('<h1> html code</h1>');
 });
 app.get('/dashboard', (req, res) =>{
     console.log('dashboard', req.method);
-    res.send('hi')
-})
+    res.send('<h1>Dashboard</h1>');
+});
 
 app.listen(PORT , () => console.log(`Server is running on port ${PORT}`));
 
