7f9487eb766a57752a21e5e870ecf7dcd34a231c
caubengungo <dangntmhe210550@gmail.com>
2026-08-29 19:33:26 +0700

    them 1 endpoint

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     console.log('lmao', req.method);
     res.sendStatus(200);
 });
+app.get('/dashboard', (req, res) =>{
+    console.log('dashboard', req.method);
+    res.send('hi')
+})
 
 app.listen(PORT , () => console.log(`Server is running on port ${PORT}`));
 
