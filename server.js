b5060434dd9a5c001aa0d8f264f771aef04fc13a
caubengungo <dangntmhe210550@gmail.com>
2026-08-30 21:13:03 +0700

    clmm

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 8383;
+
+app.use(express.json()); // Middleware to parse JSON request bodies
+let data = ['lmao'];
+
 app.get('/', (req, res) => {
     console.log('lmao', req.method);
     res.send('<h1> html code</h1>');
@@ -8,6 +12,18 @@ app.get('/', (req, res) => {
 app.get('/dashboard', (req, res) =>{
     console.log('dashboard', req.method);
     res.send('<h1>Dashboard</h1>');
+});
+app.get('/api/data', (req, res) => {
+    console.log('for data');
+    res.json(data);
+});
+
+app.post('/api/data', (req, res) => {
+    console.log('post data');
+    console.log('Request body:', req.body); // Log the request body to the console
+    data.push(req.body);
+    res.json({ message: 'Data received successfully' });
+
 });
 
 app.listen(PORT , () => console.log(`Server is running on port ${PORT}`));
