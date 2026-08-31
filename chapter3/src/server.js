366f82c3b7c62d57ba21a92a72722068be5c3fa7
caubengungo <dangntmhe210550@gmail.com>
2026-08-31 11:46:02 +0700

    them middleware va lay filename , dir url

--- chapter3/src/server.js
+++ chapter3/src/server.js
@@ -1,13 +1,23 @@
 import express from "express";
-import path from "path";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+
+//lay file name tu url
+const __filename = fileURLToPath(import.meta.url);
+//lay dirname tu file path
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT =process.env.PORT || 3000;
 
+//middleware
+app.use(express.static(path.join(__dirname, '../public')))
+
+//dung de gui giao dien html
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`);
 });
