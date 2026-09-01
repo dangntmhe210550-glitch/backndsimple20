eddc27433097f68c18672e9480a0f8718d7b3316
caubengungo <dangntmhe210550@gmail.com>
2026-09-01 14:46:12 +0700

    routes va authenticate

--- chapter3/src/server.js
+++ chapter3/src/server.js
@@ -19,6 +19,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
+//Routes
+app.use('/auth', authRoutes)
+app.use('/todos', authMiddleware, todoRoutes)
+
 app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`)
 });
