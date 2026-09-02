d532b413e1c0e4da6f1a719d0276884254fc0be5
caubengungo <dangntmhe210550@gmail.com>
2026-09-02 15:49:10 +0700

    chapter4

---
+++ chapter4/src/routes/authRoutes.js
@@ -0,0 +1,63 @@
+import express from 'express'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import db from '../db.js'
+
+const router = express.Router()
+
+router.post('/register', (req, res) => {
+    const { username , password } = req.body
+    // luu ten ng dung va encrypt passwd
+    const hashedPassword = bcrypt.hashSync(password, 8)
+    // hashed password
+
+    //them user vao db
+    try {
+        const insertUser = db.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`)
+        const result = insertUser.run(username, hashedPassword)
+
+        //add their first todo
+        const defaultTodo = `Hello :) Add your first todo`
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        //create token
+        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, {expiresIn: '24h'})
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+
+    }
+})
+
+router.post('/login', (req, res) => {
+
+    //khi login cta co email, va cbi tim passwd associated vs email do
+    //nhung no bi encrypt roi, gio cta lms de so sanh vs cai mat khau ma ho ms nhap
+    //nen minh phai encrypt cai mk ho vua nhap va so sanh vs mk trong db
+
+    const { username, password} = req.body
+
+    try {
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
+        const user = getUser(username)
+
+        if (!user) { return res.status(404).send({ message: "User not found"}) }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password)
+
+        if (!passwordIsValid) { return res.status(401).send({ message: "Invalid password"}) }
+        console.log(user)
+
+        const token = jwt.sign({ id: user.id },  process.env.JWT_SECRET, {expiresIn: '24h'})
+        res.json({ token })
+
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
+}) 
+
+
+export default router
---
+++ chapter4/src/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
+
+//lay file name tu url
+const __filename = fileURLToPath(import.meta.url);
+//lay dirname tu file path
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT =process.env.PORT || 3000;
+
+//middleware
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.json())
+
+//dung de gui giao dien html
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
+//Routes
+app.use('/auth', authRoutes)
+app.use('/todos', authMiddleware, todoRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server has started on port ${PORT}`)
+});
